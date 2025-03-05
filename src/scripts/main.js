8ced6db2846018a21d9d89015947666519a8a784
nataliia-v <nataly.verbenskaya@gmail.com>
2025-03-05 16:32:25 +0200

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write code here
+const employeeList = document.querySelector('ul');
+
+function convertSalaryToNumber(salaryStr) {
+  return parseFloat(salaryStr.replace(/[$,]/g, ''));
+}
+
+function sortList(list) {
+  const listItems = [...list.children];
+
+  listItems.sort((a, b) => {
+    const salaryA = convertSalaryToNumber(a.dataset.salary);
+    const salaryB = convertSalaryToNumber(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+
+  listItems.forEach((item) => list.appendChild(item));
+}
+
+sortList(employeeList);
